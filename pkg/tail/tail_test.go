da73de458c1bf98d3ccb592fab2b4f2699328403
alex <alex@industriallogic.com>
2018-01-17 23:33:31 -0800

    testing comparing same ids in instances w/ different priorities

--- pkg/tail/tail_test.go
+++ pkg/tail/tail_test.go
@@ -31,3 +31,12 @@ func TestCompareSameId(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestCompareSameIdDifferentPriority(t *testing.T) {
+	t1 := Tail{10, URGENT, time.Now()}
+	t2 := Tail{10, DISMISSED, time.Now()}
+
+	if t1.Compare(t2) != 0 {
+		t.Fail()
+	}
+}
